b20cb8af1670b5edbd98236dcaa9685225a80210
cameronmcnz <you@example.com>
2024-05-10 14:23:28 -0400

    'first'

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import navbar from './components/navbar/navbar'
+
+
+const App = () => {
+  return (
+    <div> 
+      <navbar/>
+
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './navbar.css'
+import logo from '../../assest/Logo_red.png'
+const navbar = () => {
+  return (
+    <nav>
+<img src={logo} alt="" />
+    <ul>
+        
+    <li>Home</li>
+    <li>Brain Storming</li>
+    <li>Placeholder</li>
+    <li>Placeholder</li>
+    <li>Placeholder</li>
+    <li>Contact us</li>
+    </ul>
+    </nav>
+      
+    
+  )
+}
+
+export default navbar
