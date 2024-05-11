760d7b8fab819008f9a10cf83ba35b86711712f0
cameronmcnz <you@example.com>
2024-05-11 13:33:02 -0400

    'titles'

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,16 @@ import React from 'react'
 import Navbar from './components/Navbar/Navbar';
 import Hero from './components/hero/Hero';
 import Programs from './components/Programs/Programs'
-
+import Title from './components/Title/Title'
 const App = () => {
   return (
     <div> 
       <Navbar/>
       <Hero/>
       <div className="container">
+        <Title subTitle='Notsureyet' title='offer'/>
         <Programs/>
-        
+
       </div>
     </div>
   )
