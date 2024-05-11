ae1230fcb0eb2d09f63272f2261065c65a64a0c6
cameronmcnz <you@example.com>
2024-05-11 13:16:06 -0400

    'thatsKewl'

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,10 @@ const App = () => {
     <div> 
       <Navbar/>
       <Hero/>
-      <Programs/>
+      <div className="container">
+        <Programs/>
+        
+      </div>
     </div>
   )
 }
--- src/components/Programs/Programs.jsx
+++ src/components/Programs/Programs.jsx
@@ -5,25 +5,45 @@ import  Flora_2 from '../../assets/Flora_2.png'
 import  Flora_3 from '../../assets/Flora_3.jpg'
 import  Flora_4 from '../../assets/Flora_4.jpg'
 import  Flora_5 from '../../assets/Flora_5.jpg'
-
+import  logo from '../../assets/logo.png'
 
 const Programs = () => {
   return (
     <div className='programs'>
       <div className='program'>
         <img src={Flora_1} alt="" />
+        <div className="caption">
+            <img src={logo} alt="" />
+            <p>Placeholder</p>
+        </div>
       </div>
       <div className='program'>
         <img src={Flora_2} alt="" />
+        <div className="caption">
+            <img src={logo} alt="" />
+            <p>Placeholder</p>
+        </div>
       </div>
       <div className='program'>
         <img src={Flora_3} alt="" />
+        <div className="caption">
+            <img src={logo} alt="" />
+            <p>Placeholder</p>
+        </div>
       </div>
       <div className='program'>
         <img src={Flora_4} alt="" />
+        <div className="caption">
+            <img src={logo} alt="" />
+            <p>Placeholder</p>
+        </div>
       </div>
       <div className='program'>
         <img src={Flora_5} alt="" />
+        <div className="caption">
+            <img src={logo} alt="" />
+            <p>Placeholder</p>
+        </div>
       </div>
     </div>
   )
