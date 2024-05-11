a77b0a67d4f4df5ab9043718a637218bc1bb98ab
cameronmcnz <you@example.com>
2024-05-11 09:20:17 -0400

    'keep_track_of_where_the_div_is'

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 import './hero.css'
 const Hero = () => {
   return (
-    <div className='hero'>
-      
+    <div className='hero container'>
+        <div className="hero-text">
+            <h1>Build you universe one world at a time</h1>
+            <p>Here is where you can keep track of every aspect of your universe.</p>
+            <button className='btn'>Placeholder</button>
+        </div>
     </div>
   )
 }
