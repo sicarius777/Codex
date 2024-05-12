f22a527a9e4462c81ac9707f0a1a5b22296d792b
cameronmcnz <you@example.com>
2024-05-12 11:26:35 -0400

    'stillNotWorking'

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -13,7 +13,10 @@ const About = () => {
       <div className="about-right">
         <h3>About the Codex</h3>
         <h2>The ultimate location to store your ideas</h2>
-        <p>My name is Roderick D'Pen welcome to the Codex. I have been writing worlds for most of my life many of those worlds were lost with the passage of time and the infinite nature of many pieces of paper.</p>
+        <p>My name is Roderick D'Pen welcome to the Codex. I have been writing worlds for most of my life many of those worlds were lost with the passage of time and the finite nature of many pieces of paper.</p>
+        <p>My inspiration for this project, comes from my favorite authors.</p>
+        <p>Brandon Sanderson has a enjoys the method of creating a worlds, its cosmology, weather, flora, fauna, and then writing a story about people in that world.</p>
+        <p>I have structured this project in to achieve the same methodology by allowing writers to create a world and build within it and then link them together to create a galaxy and eventually maybe their own universe. </p>
         <p>I am always looking to improve this site because I use it myself every day, if you have any ideas please feel free to reach out you can find my email in the contact us tab.</p>
       </div>
     </div>
