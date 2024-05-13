b0484029c99cb0d253b4775c87b5a842dbf5ba32
cameronmcnz <you@example.com>
2024-05-12 21:55:03 -0400

    'g'

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -15,9 +15,9 @@ const About = () => {
         <h2>The ultimate location to store your ideas</h2>
         <p>My name is Roderick D'Pen welcome to the Codex. I have been writing worlds for most of my life many of those worlds were lost with the passage of time and the finite nature of many pieces of paper.</p>
         <p>My inspiration for this project, comes from my favorite authors.</p>
-        <p>Brandon Sanderson enjoys the method of creating a worlds, its cosmology, weather, flora, fauna, and then writing a story about people in that world.</p>
+        <p>Brandon Sanderson enjoys the method of creating a world, its cosmology, weather, flora, fauna, ect and then writing a story about people in that world.</p>
         <p>This style of writing can also be found in the greats of Gary Gygax and Dave Arneson who created a entire game system called D&D which encourages this style of design,</p>
-        <p>I have structured this project in to achieve the same methodology by allowing writers to create a world and build within it and then link them together to create a galaxy and eventually maybe their own universe. </p>
+        <p>I have structured this project to achieve the same methodology by allowing writers to create a world and build within it and then link them together to create a galaxy and eventually maybe their own universe. </p>
         <p>I hope this project aids you in a similar way.</p>
         <p>I am always looking to improve this site because I use it myself every day, if you have any ideas please feel free to reach out you can find my email in the contact us tab.</p>
       </div>
